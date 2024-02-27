a66658922961e89496e347f8e60a6c1f41238802
Chun-yu Lin <lincy@nchc.org.tw>
2024-02-27 14:31:53 +0800

    Add reference.

--- mpi/omp_f.f90
+++ mpi/omp_f.f90
@@ -11,6 +11,7 @@ real(kind=8) :: tmp, t0,t1,t2,t3, zetam2
 !!!   1) 1/k**2 can be pull out of the loop
 !!!   2) The memory affinity matters for the testval(i,j) loop. Instead of using testval = 0.0, using OMP loop initialization to 
 !!!      ensure "First Touch Data Placement Policy" for better memory affinity 
+!!!        Ref: https://www.openmp.org/wp-content/uploads/SC18-BoothTalks-vanderPas.pdf 
 !!!   3) The performance drop at 28 core (1-socket), possiably due to wired cpu binding where one core is located into the other socket.
 !!!      We ignore it unless it really matters for the whole project.
 !!!
@@ -21,6 +22,7 @@ t0 = omp_get_wtime()
 do k = 1,ITER
    zetam2 = zetam2 + 1.0/dble(k**2)   ! dble(i*j)/dble(k**2)
 enddo
+!$omp end parallel do
 
 !! Instead of use testval = 0.0, using loop initialization to ensure "First Touch Data Placement Policy"
 !$omp parallel do shared(testval)
@@ -32,7 +34,6 @@ enddo
 !$omp end parallel do
 
 
-!! OMP scaling not good for this loop, probably due to the matrix update?
 t1= omp_get_wtime()
 !$omp parallel do shared(testval)
 do j = 1, N
@@ -57,3 +58,8 @@ t3= omp_get_wtime()
 print '(A,I3,A,3E9.2,A,E11.4)', "#Cores " , omp_get_max_threads(), "  Walltime/secs: ", t1-t0, t2-t1, t3-t2,"    Check: ", tmp
 
 end program
+
+
+
+
+! To avoid using temp variable, see https://stackoverflow.com/questions/14420740/how-to-use-reduction-on-an-array-in-fortran
